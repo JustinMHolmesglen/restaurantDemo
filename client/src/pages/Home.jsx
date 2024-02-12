2609604153dd8dfe87fdf5f1ac192e8b43700120
Justin <mca02097832@student.holmesglen.edu.au>
2024-02-12 12:12:34 +1100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,20 +14,20 @@ const Home = () => {
           <Button href={"/review"} variant="dark">Review</Button>
         </div>
         <div className="row mb-3">
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-salmon-pic.jpg" alt="Food-gallery-1" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-prawns-pic.jpg" alt="Food-gallery-2" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-rice.jpg" alt="Food-gallery-3" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/seafood-soup.jpg" alt="Food-gallery-4" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-salmon-pic.jpg" alt="Food-gallery-1" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-prawns-pic.jpg" alt="Food-gallery-2" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-rice.jpg" alt="Food-gallery-3" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/seafood-soup.jpg" alt="Food-gallery-4" /></div>
         </div>
         <div className="row mb-3">
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-banquet-pic.jpg" alt="Food-gallery-1" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-vegies-pic.jpg" alt="Food-gallery-2" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-spicy-noodles-pic.jpg" alt="Food-gallery-3" /></div>
-          <div className="col-md-3"><img src="https://webrestaurant.onrender.com/client/src/assets/Food-sushi-pic.jpg" alt="Food-gallery-4" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-banquet-pic.jpg" alt="Food-gallery-1" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-vegies-pic.jpg" alt="Food-gallery-2" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-spicy-noodles-pic.jpg" alt="Food-gallery-3" /></div>
+          <div className="col-md-3"><img src="/client/src/assets/Food-sushi-pic.jpg" alt="Food-gallery-4" /></div>
         </div>
         <div className="row mb-3">
-          <div className="col-md-6"><img src="https://webrestaurant.onrender.com/client/src/assets/spice_pic.jpg" alt="Food-gallery-2" /></div>
-          <div className="col-md-6"><img src="https://webrestaurant.onrender.com/client/src/assets/prawns_pic.jpg" alt="Food-gallery-1"/></div>
+          <div className="col-md-6"><img src="/client/src/assets/spice_pic.jpg" alt="Food-gallery-2" /></div>
+          <div className="col-md-6"><img src="/client/src/assets/prawns_pic.jpg" alt="Food-gallery-1"/></div>
         </div>
         <div id="call-to-action" className="mb-3" style={{display: "flex", flexDirection: "column"}}>
           <h3 style={{color: "#fff", textAlign: "center"}}>With the greatest reviewed restaurant in Clayton</h3>
